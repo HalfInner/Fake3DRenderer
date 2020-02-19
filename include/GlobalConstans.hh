a3b8e65e28fa3e42b939cc11c784834652d862ee
Half's Inner <kajbrz@hotmail.com>
2020-02-19 18:42:41 +0100

    Refactor chunk

---
+++ include/GlobalConstans.hh
@@ -0,0 +1,45 @@
+//
+// Created by kajbr on 19.02.2020.
+//
+
+#ifndef FAKE3DRENDERER_GLOBALCONSTANS_HH
+#define FAKE3DRENDERER_GLOBALCONSTANS_HH
+
+#include <GL/gl.h>
+#include <GL/glu.h>
+
+/* Windows globals, defines, and prototypes */
+extern HWND ghWnd;
+extern HDC ghDC;
+extern HGLRC ghRC;
+
+#define SWAPBUFFERS SwapBuffers(ghDC)
+constexpr int BLACK_INDEX = 0;
+constexpr int RED_INDEX = 13;
+constexpr int GREEN_INDEX = 14;
+constexpr int BLUE_INDEX = 16;
+constexpr int WIDTH = 300;
+constexpr int HEIGHT = 200;
+
+LONG WINAPI MainWndProc(HWND, UINT, WPARAM, LPARAM);
+
+BOOL bSetupPixelFormat(HDC);
+
+/* OpenGL globals, defines, and prototypes */
+extern GLfloat latitude, longitude, latinc, longinc;
+extern GLdouble radius;
+
+constexpr int GLOBE = 1;
+constexpr int CYLINDER = 2;
+constexpr int CONE = 3;
+
+
+GLvoid resize(GLsizei, GLsizei);
+
+GLvoid initializeGL(GLsizei, GLsizei);
+
+GLvoid drawScene(GLvoid);
+
+void polarView(GLdouble, GLdouble, GLdouble, GLdouble);
+
+#endif //FAKE3DRENDERER_GLOBALCONSTANS_HH
